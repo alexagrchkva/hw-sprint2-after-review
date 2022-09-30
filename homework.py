d63d5a973890a607be58f36d1587741533c06c90
Александра Горчакова <alx.gorchakova@gmail.com>
2022-09-30 20:49:32 +0300

    Удален список атрибутов в комментарии к классу InfoMessage Изменен комментарий к функции get_message Перенос скобок на новые строки в return Аргументы функции def.__init__ перенесены каждый на отдельную строку В функции def show_training_info(self) закрывающую скобку перенесла на следующую строку Перенесены строки в формуле calories_for_running Убраны лишние пробелы в аннотациях типов аргументов Исправлены скобки под def get_spent_calories(self) в соответствие с примером из ревью def get_spent_calories(self): return заменен на raise, NotImplemented - на NotImplementedError Удален список SUPPORTED_FORMATS Проверка “if [название тренировки] в списке поддерживаемых” перенесена в функцию def read_package(workout_type: str, data: list), выполнена в виде поиска соответствия по ключу словаря

--- homework.py
+++ homework.py
@@ -78,12 +78,12 @@ class Running(Training):
     def get_spent_calories(self) -> float:
         calories_for_running = (
                          (
-                            self.RUN_CAL_1 * self.get_mean_speed()
-                            - self.RUN_CAL_2
-                          ) * self.weight
-                          / self.M_IN_KM
-                          * (
-                           self.duration * Training.MIN_IN_HOUR
+                          self.RUN_CAL_1 * self.get_mean_speed()
+                          - self.RUN_CAL_2
+                         ) * self.weight
+                         / self.M_IN_KM
+                         * (
+                             self.duration * Training.MIN_IN_HOUR
                           )
         )
         return calories_for_running
